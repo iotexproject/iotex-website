57aa40983ba245c2fc0a0818a798af6221e96585
uuz <ncysatnaf@gmail.com>
2021-03-22 15:35:56 +0800

    add ga

--- app/pages/_document.tsx
+++ app/pages/_document.tsx
@@ -40,6 +40,21 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+          <!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-111756489-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-111756489-1');
+</script>
+          `,
+          }}
+        ></script>
         <DocumentHead />
         <body>
           <Main />
