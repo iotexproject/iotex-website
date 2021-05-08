d1204331d03637bbd11182fe09dbcbc4b9b4395e
uuz <ncysatnaf@gmail.com>
2021-05-08 11:35:37 +0800

    fix ga

--- app/pages/_document.tsx
+++ app/pages/_document.tsx
@@ -40,11 +40,11 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-111756489-1"></script>
         <script
           dangerouslySetInnerHTML={{
             __html: `
           <!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-111756489-1"></script>
 <script>
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
