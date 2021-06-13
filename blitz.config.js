ad3364c7bfc0d64607ca60e8e81e66b6f0de5d5a
uuz <ncysatnaf@gmail.com>
2021-06-14 00:15:39 +0800

    add redirects

--- blitz.config.js
+++ blitz.config.js
@@ -9,6 +9,30 @@ module.exports = {
       return next()
     },
   ],
+  async redirects() {
+    return [
+      { source: "/pebble-tracker", destination: "/pebble" },
+      { source: "/discord", destination: "https://discord.gg/KTHUKBBW87" },
+      { source: "/devdiscord", destination: "https://discord.gg/q5eYde2CU7" },
+      {
+        source: "/white-paper",
+        destination: "http://files.iotex.io/publications/IoTeX_Whitepaper_1.5_EN.pdf",
+      },
+      {
+        source: "/reference-architecture",
+        destination: "https://iotex.b-cdn.net/docs/reference-architecture.pdf",
+      },
+      {
+        source: "/images/:match*",
+        destination:
+          "https://github.iotex.io/iotexproject/iotex-website/main/public/images/:match*",
+      },
+      {
+        source: "/exchanges",
+        destination: "https://ecosystem.iotex.io/exchanges-wallets",
+      },
+    ]
+  },
   /*
   webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
     // Note: we provide webpack above so you should not `require` it
