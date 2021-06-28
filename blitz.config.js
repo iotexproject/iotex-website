a8627f6c1c042c9c284938b8624a525bd7a8113f
uuz <ncysatnaf@gmail.com>
2021-06-28 18:29:59 +0800

    update redirect

--- blitz.config.js
+++ blitz.config.js
@@ -14,7 +14,7 @@ module.exports = {
     return [
       {
         source: "/blog/:match*",
-        destination: "https://blog.iotex.io/:match*",
+        destination: "https://iotex.io/blog/:match*",
       },
     ]
   },
