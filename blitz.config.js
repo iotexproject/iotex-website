12cbe7c228b386a0cb4a61a155c26aaaa4838432
uuz <ncysatnaf@gmail.com>
2021-06-28 19:41:34 +0800

    update

--- blitz.config.js
+++ blitz.config.js
@@ -18,7 +18,7 @@ module.exports = {
       },
       {
         source: "/blog/:match*",
-        destination: "https://iotex-blog.onrender.com/blog/:match*/",
+        destination: "https://iotex-blog.onrender.com/blog/:match*",
       },
     ]
   },
